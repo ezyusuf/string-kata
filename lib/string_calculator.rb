9b7df5c847eca7accb35ce228ee7059e6d93143a
Ezaam Yusuf <ezaamyusuf@gmail.com>
2015-06-25 02:20:57 -0400

    Passed all 8 tests

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,11 +1,46 @@
 module StringCalculator
 
   def self.add(string)
-    # 0
-    # string.to_i
-    string == "5" ? 5 : 0
-    # code to be tested goes here...
+    # string = set_delimiter(string)
+    # values = string.split(/[,\n]/)
+    # values = string.split(/[#{delimiter}\n]/).map(&:to_i)
+    # raise RuntimeError if values.detect { |v| v < 0}
+    values = extract_values(string)
+    raise RuntimeError if values.detect { |v| v < 0}
+    values.reduce(:+) || 0
+    # values.map(&:to_i).reduce(:+) || 0
   end
+
+  def self.extract_values(string)
+    delimiter = extract_delimiter(string)
+    string.split(/[#{delimiter}\n]/).map(&:to_i)
+  end
+
+  def self.extract_delimiter(string)
+    # delimiter = string =~ /^\/\// ? string[2] : ","
+    string =~ /^\/\// ? string[2] : ","
+  end
+
+  # def self.set_delimiter(string)
+  #   # if string =~ /^\/\//
+  #   #   delimiter = string[2]
+  #   # end
+  #   delimiter = string =~ /^\/\// ? string[2] : ","
+  # end
+
+  # def self.add(string)
+  #   # 0
+  #   # string.to_i
+  #   if string.include?(",")
+  #     values = string.split(/[,\n]/)
+  #     values.map(&:to_i).reduce(:+) || 0
+  #     # values.inject(0) { |s,v| s += v.to_i}
+
+  #   else
+  #     string == "5" ? 5 : 0
+  #   end
+  #   # code to be tested goes here...
+  # end
 
   # ...and here when the above becomes too complex.
 
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -22,5 +22,31 @@ RSpec.describe StringCalculator, "#add" do
     end
   end
 
+  context "two values" do
+    it "returns 3 for string '1,2'" do
+      expect(StringCalculator.add("1,2")).to eql(3)
+    end
+  end
+
+  context "handle various delimiters" do
+    it "returns 6 for string '1\n2,3'" do
+      expect(StringCalculator.add("1\n2,3")).to eql(6)
+    end
+
+    it "returns 7 for string '//;\n2;5'" do
+      expect(StringCalculator.add("//;\n2;5")).to eql(7)
+    end
+  end
+
+  context "negative numbers" do
+    it "throws a RuntimeError when negative numbers exists " do
+      expect {StringCalculator.add("-1")}.to raise_error(RuntimeError)
+    end
+    it "RuntimeError message contains negative numbers if more than one exists " do
+      expect {StringCalculator.add("-1")}.to raise_error(RuntimeError)
+    end
+  end
+
+
   # more tests go here
 end
